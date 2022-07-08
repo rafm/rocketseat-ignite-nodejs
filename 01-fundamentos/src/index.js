43071a7c17a6a74944ae372e9f2ecb3fb7e89611
Rafael Maia <rafael.maia@arctouch.com>
2022-07-08 19:10:34 -0300

    Configuring JSON middleware on Express to allow receiving JSON in request body

--- 01-fundamentos/src/index.js
+++ 01-fundamentos/src/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/courses", (request, response) => {
     return response.json(["Curso 1", "Curso 2", "Curso 3"])
 })
