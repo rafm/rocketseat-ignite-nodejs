767bc3ee98164188d8dd144e7ae6b7a6e3c15bf4
Rafael Maia <rafael.maia@arctouch.com>
2022-08-03 17:14:50 -0300

    Creating POST endpoint "/withdraw" to create a withdraw (debit) statement operation in an account

--- 01-fundamentos/src/index.js
+++ 01-fundamentos/src/index.js
@@ -7,6 +7,19 @@ app.use(express.json())
 
 const customers = []
 
+function getBalance(statement) {
+    return statement.reduce((acc, operation) => {
+        switch (operation.type) {
+            case "credit":
+                return acc + operation.amount
+            case "debit":
+                return acc - operation.amount
+            default:
+                return acc
+        }
+    }, 0)
+}
+
 // Middleware
 function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.headers
@@ -58,6 +71,27 @@ app.post("/deposit", (request, response) => {
         amount,
         created_at: new Date(),
         type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).json(statementOperation)
+})
+
+app.post("/withdraw", (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
     }
 
     customer.statement.push(statementOperation)
